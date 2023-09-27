2e5df9a0b6a98bae88e87014a064df43482d81e0
kellycesario <kellycesario@gmail.com>
2023-09-27 06:31:00 -0300

    feat: importação global dos estilos

--- src/web/next.config.js
+++ src/web/next.config.js
@@ -1,4 +1,14 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, '.styles')],
+    prependData: `@import "./styles/index.scss";`,
+  },
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
+}
 
 module.exports = nextConfig
