07ddfbec61bdd312082d59420a73605665ecc569
kellycesario <kellycesario@gmail.com>
2023-10-05 09:11:09 -0300

    feat: criação do banner download

---
+++ src/web/components/organisms/Download/index.stories.tsx
@@ -0,0 +1,12 @@
+import { DownloadBanner } from './index'
+
+export default {
+  component: DownloadBanner,
+  title: 'Organisms/DownloadBanner',
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+}
+
+export const Default = {}
