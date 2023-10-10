c15238fd87568263560fc6b8ef324c2a453546f2
kellycesario <kellycesario@gmail.com>
2023-10-10 20:42:54 -0300

    feat: adiciona o componente goal

---
+++ src/web/components/molecules/Goal/index.stories.tsx
@@ -0,0 +1,37 @@
+import { Sprites } from '@/components/ions/Sprites'
+import type { Meta, StoryObj } from '@storybook/react'
+import { Goal } from '.'
+
+const goal = {
+  title: 'Molecules/Goal',
+  component: Goal,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    goal: 'No live organism can continue for long to exist under conditions of absolute reality; even larks and katydids are supposed, by some, to dream.',
+  },
+  argTypes: {
+    goal: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <div>
+          <Sprites />
+          <Story />
+        </div>
+      )
+    },
+  ],
+} satisfies Meta<typeof Goal>
+
+export default goal
+
+type Story = StoryObj<typeof goal>
+export const Primary: Story = {}
--- src/web/components/molecules/Goal/index.tsx
+++ src/web/components/molecules/Goal/index.tsx
@@ -1,5 +1,37 @@
-export const x = () => {
+'use client'
+
+import { Button } from '@/components/atoms/Button'
+import { useState } from 'react'
+import styles from './styles.module.scss'
+
+interface GoalProps {
+  goal?: string
+}
+
+export const Goal = ({ goal }: GoalProps) => {
+  const [isChecked, setIsChecked] = useState(false)
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked)
+  }
+
   return (
-    <></>
+    <label className={styles.goal} htmlFor={goal}>
+      <input
+        type='checkbox'
+        className={styles.goal__input}
+        onChange={handleCheckboxChange}
+        id={goal}
+      />
+      <p className={styles.goal__container}>
+        <span className={styles.goal__text}>{goal}</span>
+      </p>
+      <Button
+        hasIcon={true}
+        icon={isChecked ? 'trash' : 'pencil'}
+        size='small'
+        className={styles.goal__button}
+      />
+    </label>
   )
 }
