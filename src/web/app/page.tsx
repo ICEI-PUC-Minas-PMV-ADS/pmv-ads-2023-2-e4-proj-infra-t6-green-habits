8b7ffa252d07f9a57a82eb5d737c90037880b662
kellycesario <kellycesario@gmail.com>
2023-10-10 17:46:15 -0300

    feat: importação e ajustes de componentes em páginas já existentes

--- src/web/app/page.tsx
+++ src/web/app/page.tsx
@@ -1,10 +1,12 @@
 import { Banner } from '@/components/organisms/Banner'
 import { DownloadBanner } from '@/components/organisms/Download'
 import styles from './homepage.module.scss'
+import { HabitsWrapper } from '@/components/organisms/HabitsWrapper'
 
 export default function Home() {
   return (
     <main className={styles.homepage}>
+      <HabitsWrapper />
       <DownloadBanner />
       <Banner image='/banner.png' />
     </main>
