0aa7c03cc9ca85a295fcbd2258330b4bbff23710
kellycesario <kellycesario@gmail.com>
2023-10-10 17:46:47 -0300

    feat: adição de prop backgroundColor

--- src/web/components/atoms/Tag/index.tsx
+++ src/web/components/atoms/Tag/index.tsx
@@ -2,11 +2,13 @@ import styles from './styles.module.scss'
 
 interface TagProps {
   category?: string
+  backgroundColor?: string
 }
 
-export const Tag = ({ category }: TagProps) => {
+export const Tag = ({ category, backgroundColor }: TagProps) => {
+  const classList = [styles.tag, styles[`tag--${backgroundColor}`]].join(' ')
   return (
-    <div className={styles.tag}>
+    <div className={classList}>
       <p className={styles.tag__category}>{category}</p>
     </div>
   )
