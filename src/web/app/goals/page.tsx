e0e26c269d483e90001b56c2cbeae56e9b901cee
kellycesario <kellycesario@gmail.com>
2023-10-17 19:16:57 -0300

    feat: upload de componentes nas páginas goals e habits

--- src/web/app/goals/page.tsx
+++ src/web/app/goals/page.tsx
@@ -1,9 +1,12 @@
+import { GoalsWrapper } from '@/components/organisms/GoalsWrapper'
+import { Ranking } from '@/components/organisms/Ranking'
 import styles from './styles.module.scss'
 
 export default function Goals() {
-    return (
-      <main className={styles.goals}>
-      </main>
-    )
-  }
-  
+  return (
+    <main className={styles.goals}>
+      <GoalsWrapper />
+      <Ranking />
+    </main>
+  )
+}
--- src/web/app/habits/page.tsx
+++ src/web/app/habits/page.tsx
@@ -1,12 +1,13 @@
 import { Banner } from '@/components/organisms/Banner'
 import { HabitsWrapper } from '@/components/organisms/HabitsWrapper'
+import { MyHabits } from '@/components/organisms/MyHabits'
 import styles from './styles.module.scss'
 
 export default function Habits() {
   return (
     <main className={styles.habits}>
       <HabitsWrapper />
-
+      <MyHabits />
       <Banner image='/banner.png' />
     </main>
   )
