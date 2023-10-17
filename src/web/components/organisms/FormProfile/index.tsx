159a67cbfb89231fd6ec1aeb424fbbf83839fc11
kellycesario <kellycesario@gmail.com>
2023-10-17 11:02:53 -0300

    feat: adição do heading da página profile

--- src/web/components/organisms/FormProfile/index.tsx
+++ src/web/components/organisms/FormProfile/index.tsx
@@ -1,42 +1,57 @@
 'use client'
 
 import { Button } from '@/components/atoms/Button'
+import { Heading } from '@/components/atoms/Heading'
+import { Text } from '@/components/atoms/Text'
 import { Input } from '@/components/molecules/Input'
 import formProfile from '@/data/formProfile.json'
 import styles from './styles.module.scss'
 
 export const FormProfile = () => {
   return (
-    <form className={styles.formProfile}>
-      {formProfile.map((item, index) => (
-        <Input
-          key={index}
-          id={item.id}
-          label={item.label}
-          placeholder={item.placeholder}
-          type={item.type}
-          icon={item.icon}
-          backgroundColor='gray'
-          color='white'
-        />
-      ))}
+    <section className={styles.formProfile}>
+      <Heading
+        align='center'
+        children='Editar perfil'
+        color='black'
+        level='2'
+      />
+      <Text
+        align='center'
+        children='Altere ou exclua seu perfil nesta página'
+        color='black'
+      />
+      <form className={styles.formProfile}>
+        {formProfile.map((item, index) => (
+          <Input
+            key={index}
+            id={item.id}
+            label={item.label}
+            placeholder={item.placeholder}
+            type={item.type}
+            icon={item.icon}
+            backgroundColor='gray'
+            color='white'
+          />
+        ))}
 
-      <div className={styles.formProfile__buttons}>
-        <Button
-          isButton
-          label='Salvar alterações'
-          level='primary'
-          hasIcon
-          icon='check-01'
-        />
-        <Button
-          isButton
-          label='Excluir conta'
-          level='tertiary'
-          hasIcon
-          icon='trash'
-        />
-      </div>
-    </form>
+        <div className={styles.formProfile__buttons}>
+          <Button
+            isButton
+            label='Salvar alterações'
+            level='primary'
+            hasIcon
+            icon='check-01'
+          />
+          <Button
+            isButton
+            label='Excluir conta'
+            level='tertiary'
+            hasIcon
+            icon='trash'
+          />
+        </div>
+      </form>
+    </section>
   )
 }
