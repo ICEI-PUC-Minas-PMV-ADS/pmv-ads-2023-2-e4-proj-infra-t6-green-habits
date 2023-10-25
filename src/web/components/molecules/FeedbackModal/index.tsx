506946f2e4ba97e1b274107402d05f292eae4e30
kellycesario <kellycesario@gmail.com>
2023-10-25 01:21:31 -0300

    feat: atualização do componente FeedbackModal

--- src/web/components/molecules/FeedbackModal/index.tsx
+++ src/web/components/molecules/FeedbackModal/index.tsx
@@ -1,7 +1,23 @@
 'use client'
 
+import { Icon } from '@/components/atoms/Icon'
+import { Text } from '@/components/atoms/Text'
 import styles from './styles.module.scss'
 
-export const FeedBackModal = () => {
-  return <div className={styles.modal}></div>
+interface FeedBackModalProps {
+  success: boolean
+  error: boolean
+  text?: string
+}
+
+export const FeedBackModal = ({ success, error, text }: FeedBackModalProps) => {
+  const successText = 'Operação realizada com sucesso!'
+  const errorText = 'Ocorreu um erro durante a operação.'
+  return (
+    <div className={styles.modal}>
+      <Icon icon='tulip' fill='#398278' />
+      {success && <Text>{text || successText}</Text>}
+      {error && <Text>{text || errorText}</Text>}
+    </div>
+  )
 }
