dc1680f1fc582ad78e4a94b955d7d1762a26026d
kellycesario <kellycesario@gmail.com>
2023-11-03 08:08:24 -0300

    feat: upload de configuração inicial da aplicação mobile

---
+++ src/mobile/src/components/organisms/FormProfile/index.js
@@ -0,0 +1,9 @@
+import { View } from 'react-native'
+
+export const FormProfile = () => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
---
+++ src/mobile/src/components/organisms/FormRegister/index.js
@@ -0,0 +1,9 @@
+import { View } from 'react-native'
+
+export const FormRegister = () => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
