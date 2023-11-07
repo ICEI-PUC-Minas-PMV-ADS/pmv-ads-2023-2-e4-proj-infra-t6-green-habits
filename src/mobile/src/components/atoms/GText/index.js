90b0b927e7f68beec7f678157a226753524acc37
kellycesario <kellycesario@gmail.com>
2023-11-07 00:08:24 -0300

    feat: configuração de fontes nos componentes GText e Title

--- src/mobile/src/components/atoms/GText/index.js
+++ src/mobile/src/components/atoms/GText/index.js
@@ -1,12 +1,43 @@
 import { Text, View } from 'react-native'
 import styles from './styles.js'
 
+import {
+  Poppins_200ExtraLight,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  useFonts,
+} from '@expo-google-fonts/poppins'
+
 export const GText = ({ text, color }) => {
   const textColor = styles[color]
 
-  return (
-    <View>
-      <Text style={[styles.text, textColor]}>{text}</Text>
-    </View>
-  )
+  let [fontsLoaded] = useFonts({
+    Poppins_200ExtraLight,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  })
+
+  if (fontsLoaded) {
+    return (
+      <View>
+        <Text
+          style={[
+            styles.text,
+            textColor,
+            { fontFamily: 'Poppins_200ExtraLight' },
+          ]}
+        >
+          {text}
+        </Text>
+      </View>
+    )
+  } else {
+    return null
+  }
 }
--- src/mobile/src/components/atoms/Title/index.js
+++ src/mobile/src/components/atoms/Title/index.js
@@ -1,11 +1,43 @@
 import { Text, View } from 'react-native'
 import styles from './styles.js'
 
+import {
+  Poppins_200ExtraLight,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  useFonts,
+} from '@expo-google-fonts/poppins'
+
 export const Title = ({ title, color }) => {
+  let [fontsLoaded] = useFonts({
+    Poppins_200ExtraLight,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  })
+  
   const titleColor = styles[color]
-  return (
-    <View>
-      <Text style={[styles.title, titleColor]}>{title}</Text>
-    </View>
-  )
+
+  if (fontsLoaded) {
+    return (
+      <View>
+        <Text
+          style={[
+            styles.title,
+            titleColor,
+            { fontFamily: 'Poppins_400Regular' },
+          ]}
+        >
+          {title}
+        </Text>
+      </View>
+    )
+  } else {
+    return null
+  }
 }
