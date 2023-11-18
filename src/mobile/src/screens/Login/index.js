46e6e65b94bf05595f692de7c97ceff4c90acac9
jnunesss <103083123+jnunesss@users.noreply.github.com>
2023-11-18 10:19:32 -0300

    feat: redirecionar se logado

--- src/mobile/src/screens/Login/index.js
+++ src/mobile/src/screens/Login/index.js
@@ -1,8 +1,21 @@
 import { ScrollView } from 'react-native'
 import { FormLogin } from '../../components/organisms/FormLogin'
 import { styles } from './styles'
+import { useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useNavigation } from '@react-navigation/native'
 
 export const LoginPage = () => {
+  const navigation = useNavigation()
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        navigation.navigate('Tabs', { screen: 'Home' })
+      }
+    }
+    checkToken()
+  }, [])
   return (
     <ScrollView style={styles.container}>
       <FormLogin />
